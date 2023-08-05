eaffe56ab356821303a14e5d9e2ed50fe3042e2f
TuTuanLe <tutuanletkpt@gmail.com>
2023-08-05 20:14:47 +0700

    feat: add pineline deploy to dev

--- src/utils/connect.ts
+++ src/utils/connect.ts
@@ -3,9 +3,8 @@ import config from 'config';
 import logger from './logger';
 
 async function connect() {
-  // const dbUri = config.get<string>('dbUri');
-  const dbUri = 'mongodb+srv://tutuanle:5KceUHEmgGf2Brct@money-keeper.zuvtb7i.mongodb.net/?retryWrites=true&w=majority';
-  logger.info('🚀 DB_URI = ', dbUri);
+  const dbUri = config.get<string>('dbUri');
+  logger.info(`🚀 DB_URI = ${dbUri} `);
   try {
     await mongoose.connect(dbUri);
     logger.info('DB connected');
