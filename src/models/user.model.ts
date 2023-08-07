01edc0bee8da61a6250344bdffd10e25ed6246f1
phanvohieunghia <phvohieunghia@gmail.com>
2023-08-07 17:26:26 +0700

    feat: install husky v3

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -46,7 +46,7 @@ userSchema.pre('save', async function (next) {
 userSchema.methods.comparePassword = async function (candidatePassword: string): Promise<boolean> {
   const user = this as UserDocument;
 
-  return bcrypt.compare(candidatePassword, user.password).catch((e) => false);
+  return bcrypt.compare(candidatePassword, user.password).catch(e => false);
 };
 
 const UserModel = mongoose.model<UserDocument>('User', userSchema);
