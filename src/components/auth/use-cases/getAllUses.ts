e6765f96371ec0a1f8ca7e742bd483833a91b04f
phanvohieunghia <phvohieunghia@gmail.com>
2023-08-07 17:24:17 +0700

    feat: install husky v2

--- src/components/auth/use-cases/getAllUses.ts
+++ src/components/auth/use-cases/getAllUses.ts
@@ -5,7 +5,7 @@ import { FindAllUserResponse, User } from '../interface';
 export async function getAllUsers(): Promise<FindAllUserResponse> {
   try {
     const users = await UserModel.find().lean().exec();
-    const transformedUsers: User[] = users.map((user) => {
+    const transformedUsers: User[] = users.map(user => {
       const { _id, __v, password, ...rest } = user;
       return { id: _id, ...rest };
     });
