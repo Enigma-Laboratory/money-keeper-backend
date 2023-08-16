d45136a878acc9098b263cca553e2a5d533c59cb
phanvohieunghia <phvohieunghia@gmail.com>
2023-08-16 20:57:10 +0700

    feature/EL-6 v3
    (cherry picked from commit 956a9cafe035413083d9c78bb0d0462a986bb7c8)

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,8 +5,7 @@ import ProductRoute from './product/productRoutes';
 import OrderRoute from './order/orderRoutes';
 import CustomerRoute from './customer/customerRoutes';
 import AdditionalHttpStatusCodes from './serverError/serverError';
-import * as swaggerUi from '../utils/api-doc';
-import swaggerSpec from '../utils/api-doc';
+import swaggerUi, { swaggerSpec } from '../utils/api-doc';
 
 function routes(app: Express) {
   app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
