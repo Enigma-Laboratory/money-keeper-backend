c798f91f933f85184de12db3d78ba5eeb0223f6c
phanvohieunghia <phvohieunghia@gmail.com>
2023-08-16 20:29:35 +0700

    feature/EL-6 v2

--- src/utils/api-doc.ts
+++ src/utils/api-doc.ts
@@ -1,7 +1,7 @@
-import swaggerJsdoc from 'swagger-jsdoc';
+import swaggerJsdoc, { Options } from 'swagger-jsdoc';
 import swaggerUi from 'swagger-ui-express';
 
-const options: swaggerJsdoc.Options = {
+const options: Options = {
   definition: {
     openapi: '3.1.0',
     info: {
