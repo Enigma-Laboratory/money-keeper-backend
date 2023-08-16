ff0b2873167d1476014abe361b7e5c80319d50ba
phanvohieunghia <phvohieunghia@gmail.com>
2023-08-16 22:22:25 +0700

    feature/EL-6/create class for swagger
    (cherry picked from commit 2f6e4b351da6f06db7b8d0fc48b9b11c701e2ec2)

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,10 +5,10 @@ import ProductRoute from './product/productRoutes';
 import OrderRoute from './order/orderRoutes';
 import CustomerRoute from './customer/customerRoutes';
 import AdditionalHttpStatusCodes from './serverError/serverError';
-import swaggerUi, { swaggerSpec } from '../utils/api-doc';
+import SwaggerUI from '../utils/api-doc';
 
 function routes(app: Express) {
-  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+  app.use('/api-docs', SwaggerUI.instance.serve, SwaggerUI.instance.setup());
   app.use('/auth', AuthRoute);
   app.use('/session', SessionRoute);
   app.use('/product', ProductRoute);
--- src/utils/api-doc.ts
+++ src/utils/api-doc.ts
@@ -1,25 +1,45 @@
 import swaggerJsdoc from 'swagger-jsdoc';
 import swaggerUi from 'swagger-ui-express';
+import { RequestHandler } from 'express';
 
-const options: swaggerJsdoc.Options = {
-  definition: {
-    openapi: '3.1.0',
-    info: {
-      title: 'NodeJS API Project for mongodb',
-      version: '1.0.0',
-    },
-    servers: [{ url: 'http://localhost:1338', description: 'Development server' }],
-    tags: [
-      { name: 'user', description: '' },
-      { name: 'auth', description: '' },
-      { name: 'order', description: '' },
-      { name: 'order detail', description: '' },
-      { name: 'product', description: '' },
-      { name: 'session', description: '' },
-    ],
-  },
-  apis: ['./src/routes/**/*.ts'],
-};
-const swaggerSpec = swaggerJsdoc(options);
-export { swaggerSpec };
-export default swaggerUi;
+export default class SwaggerUI {
+  private static _instance: SwaggerUI;
+  private options: Options;
+  private swaggerSpec: object;
+  serve: RequestHandler[];
+
+  public static get instance(): SwaggerUI {
+    if (!SwaggerUI._instance) {
+      this._instance = new this();
+    }
+    return this._instance;
+  }
+
+  constructor() {
+    this.serve = swaggerUi.serve;
+    this.options = {
+      definition: {
+        openapi: '3.1.0',
+        info: {
+          title: 'NodeJS API Project for mongodb',
+          version: '1.0.0',
+        },
+        servers: [{ url: 'http://localhost:1338', description: 'Development server' }],
+        tags: [
+          { name: 'user', description: '' },
+          { name: 'auth', description: '' },
+          { name: 'order', description: '' },
+          { name: 'order detail', description: '' },
+          { name: 'product', description: '' },
+          { name: 'session', description: '' },
+        ],
+      },
+      apis: ['./src/routes/**/*.ts'],
+    };
+    this.swaggerSpec = swaggerJsdoc(this.options);
+  }
+
+  public setup() {
+    return swaggerUi.setup(this.swaggerSpec);
+  }
+}
