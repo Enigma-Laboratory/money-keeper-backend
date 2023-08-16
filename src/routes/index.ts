2f6e4b351da6f06db7b8d0fc48b9b11c701e2ec2
phanvohieunghia <phvohieunghia@gmail.com>
2023-08-16 22:22:25 +0700

    feature/EL-6/create class for swagger

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,10 +2,10 @@ import { Express } from 'express';
 import AuthRoute from './auth/authRoutes';
 import HavingTokenRoute from './token/index';
 import AdditionalHttpStatusCodes from './serverError/serverError';
-import swaggerUi, { swaggerSpec } from '../utils/api-doc';
+import SwaggerUI from '../utils/api-doc';
 
 function routes(app: Express) {
-  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+  app.use('/api-docs', SwaggerUI.instance.serve, SwaggerUI.instance.setup());
   app.use('/auth', AuthRoute);
   app.use('/', HavingTokenRoute);
   app.use(AdditionalHttpStatusCodes);
