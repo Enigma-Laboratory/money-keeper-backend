a35277bc231a509fbf238ffddff6f51cb83a2d2c
TuTuanLe <tutuanletkpt@gmail.com>
2024-05-17 21:14:09 +0700

    feat: add enum user event

--- packages/shared/src/interfaces/user/user.types.ts
+++ packages/shared/src/interfaces/user/user.types.ts
@@ -6,4 +6,9 @@ export enum UserTypes {
 export enum UserEvent {
   CREATED = 'user:created',
   UPDATED = 'user:updated',
+  DELETED = 'order:deleted',
+  CREATE = 'order:create',
+  UPDATE = 'order:update',
+  READ = 'order:read',
+  DELETE = 'order:delete',
 }
