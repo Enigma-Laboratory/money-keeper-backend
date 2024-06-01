55af0d5a21f5ea2fa904f0d1f6ea3e227291bbb1
TuTuanLe <tutuanletkpt@gmail.com>
2024-06-01 11:49:59 +0700

    fix: cors with pencusto

--- packages/server-api/src/utils/cors.ts
+++ packages/server-api/src/utils/cors.ts
@@ -1,4 +1,4 @@
-const whitelist = ['http://localhost:3000', 'http://localhost:4000', 'https://pencusto-dev.netifly.app'];
+const whitelist = ['http://localhost:3000', 'http://localhost:4000', 'https://pencusto-dev.netlify.app'];
 
 const corsOptions = (req: any, callback: Function): void => {
   const origin = req.headers.origin;
